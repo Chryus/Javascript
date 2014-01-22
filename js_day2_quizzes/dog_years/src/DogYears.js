c29352e53e166ee975ee1032e885385074ae23b8
Chris <chris.haack@me.com>
2014-01-22 08:50:36 -0500

    add solution for dog years

--- js_day2_quizzes/dog_years/src/DogYears.js
+++ js_day2_quizzes/dog_years/src/DogYears.js
@@ -1,2 +1,27 @@
 //create an object called dog
 //it should have 3 functions, ageInYears, ageInDays, ageInDogYears
+
+var date = new Date();
+var birth_year = Math.floor(Math.random() * (2013 - 1914 + 1)) + 1914;
+var year = date.get_Full_Year;
+var age = year - birth_year
+
+
+function Dog(age) {
+  this._age = age ;
+}
+
+Dog.prototype.ageInYears = function fn1() {
+  return (year - birth_year);
+}
+
+Dog.prototype.ageInDays = function fn1() {
+  return this.ageInYears * 365;
+}
+
+Dog.prototype.ageInDogYears = function fn1() {
+  return this.ageInYears * 7;
+}
+
+// Dog.prototype.roll = function fn1() {
+//   return this._age
