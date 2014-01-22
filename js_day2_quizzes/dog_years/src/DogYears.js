591f16062e5c7ecc411426329336dbf4ec782807
Chris <chris.haack@me.com>
2014-01-22 09:17:41 -0500

    refactor code to utilize date/time classes

--- js_day2_quizzes/dog_years/src/DogYears.js
+++ js_day2_quizzes/dog_years/src/DogYears.js
@@ -1,27 +1,26 @@
 //create an object called dog
 //it should have 3 functions, ageInYears, ageInDays, ageInDogYears
 
-var date = new Date();
-var birth_year = Math.floor(Math.random() * (2013 - 1914 + 1)) + 1914;
-var year = date.get_Full_Year;
-var age = year - birth_year
+var birthYear = function() {
+  return Math.floor(Math.random() * (2013 - 1914 + 1) + 1914);
+};
+var year = function() {
+  var date = new Date();
+  return date.getFullYear();
+};
 
-
-function Dog(age) {
-  this._age = age ;
+function Dog() {
+  this.age = year() - birthYear();
 }
 
-Dog.prototype.ageInYears = function fn1() {
-  return (year - birth_year);
+Dog.prototype.ageInYears = function fn2() {
+  return this.age
 }
 
-Dog.prototype.ageInDays = function fn1() {
-  return this.ageInYears * 365;
+Dog.prototype.ageInDays = function fn3() {
+  return this.ageInYears() * 365;
 }
 
-Dog.prototype.ageInDogYears = function fn1() {
-  return this.ageInYears * 7;
+Dog.prototype.ageInDogYears = function fn4() {
+  return this.ageInYears() * 7;
 }
-
-// Dog.prototype.roll = function fn1() {
-//   return this._age
