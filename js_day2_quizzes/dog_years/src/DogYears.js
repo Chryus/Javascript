fa8ad48997d9f90acf144efcbd28a1b97cdd403f
Chris <chris.haack@me.com>
2014-01-25 19:39:51 -0500

    refactor animal constructor

--- js_day2_quizzes/dog_years/src/DogYears.js
+++ js_day2_quizzes/dog_years/src/DogYears.js
@@ -2,31 +2,43 @@
 //it should have 3 functions, ageInYears, ageInDays, ageInDogYears
 
 //Animal class
-var year = function () {
-    var date = new Date();
-    return date.getFullYear();
-};
+// var year = function () {
+//     var date = new Date().get;
+//     return date.getFullYear();
+// };
 
+//animal constructor
 function Animal() {
     this.birthYear = Math.floor(Math.random() * (2013 - 1985 + 1) + 1985);
-    this.age = year() - this.birthYear;
+    this.age = new Date().getFullYear() - this.birthYear;
 }
 
+//animal object functions
+//put the methods here because if we put them in the constructor
+//each new animal we create will get a function and that's bad for memory
+//we just want it to inherit a reference to a method so we use prototype below
 Animal.prototype.eat = function (food) {
     console.log("munch munch munch " + food);
 };
 
+Animal.prototype.bithYear = function() {
+  return Math.floor(Math.random() * (2013 - 1985 + 1) + 1985);
+}
+
 
 //define the Dog class
+//this whole function is a constructor
 function Dog() {
     //call the parent constructor
     Animal.call(this);
 }
 
 //inherit Animal
+//the protoypte of a dog is an animal all dogs have a basic understanding of dinosaur
 Dog.prototype = new Animal();
 
 // correct the constructor pointer because it points to Animal
+// we wanted everything in from the animal besides the constructor
 Dog.prototype.constructor = Dog;
 
 //these are object methods!
@@ -42,7 +54,8 @@ Dog.prototype.ageInDogYears = function () {
     return this.ageInYears() * 7;
 };
 
-// var myDog = new Dog();
+var myDog = new Dog();
+
 
 
 // > var myDog = new Dog();
